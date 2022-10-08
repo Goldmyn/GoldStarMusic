01b327359410ed5ddfddff3d1f6d7f84afe233a1
Marius Nwokolo <mariusnwokolo24@gmail.com>
2022-10-08 03:18:28 -0600

    deleted:    .history/src/redux/services/shazamCore_20220930135110.js

--- .history/src/components/ArtistCard_20221002075251.jsx
+++ .history/src/components/ArtistCard_20221002075251.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 const ArtistCard = ({ track }) => {
   const navigate = useNavigate();
